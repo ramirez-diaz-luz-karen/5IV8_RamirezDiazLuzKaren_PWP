b7d4b8d1f54951714c887213b7ad39e53b31cd49
5IV8_RamirezDiazLuzKaren_PWP <lramirezd2302@alumno.ipn.mx>
2025-10-08 18:53:31 -0600

    Se agrego el formato paa los problemas

---
+++ 05Ejercicios/js/funcionesE1.js
@@ -0,0 +1,26 @@
+function validarn(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if (teclado==8) return true;
+    var patron = /[0-9\d .]/;
+
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+//Funcion para calcular el interes
+function interes(){
+    var valor = document.getElementById("cantidadi").value;
+
+    var parseo = parseFloat(valor);
+    alert(parseo);
+    var interes = (parseo * 0.085);//LIMITE A 2 DECIMALES
+    alert(interes);
+    var total = interes + parseo;
+    alert(total);
+    document.getElementById("saldoi").value = "$ " + total; //LIMITE A 2 DECIMALES
+}
+
+function Borrar(){
+    document.getElementById("cantidadi").value = "";
+    document.getElementById("salodoi").value = "";
+}
---
+++ 06Algoritmia/js/algoritmia.js
@@ -0,0 +1,18 @@
+function problema1() {
+    //tarea
+
+}
+
+
+
+function problema2() {
+    
+
+}
+
+
+
+function problema3() {
+    //tarea
+
+}
