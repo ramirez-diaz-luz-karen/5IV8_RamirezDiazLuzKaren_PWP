32ef37ca40219bbdd5b29a8310d505b4d6a0fd02
5IV8_RamirezDiazLuzKaren_PWP <lramirezd2302@alumno.ipn.mx>
2025-10-12 22:06:07 -0600

    se creo el js para el primer ejercicio de algoritmia

--- 06Algoritmia/js/algoritmia.js
+++ 06Algoritmia/js/algoritmia.js
@@ -1,6 +1,32 @@
-function problema1() {
-    //tarea
 
+function problema1() {
+    const input = document.getElementById("p1-input");
+    const cadena = input.value.trim();
+
+   
+    if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/.test(cadena)) {
+        document.getElementById("resultadoC").value = "Solo se permiten letras y espacios.";
+        return;
+    }
+
+    if (cadena.includes("  ")) {
+        document.getElementById("resultadoC").value = "No se permiten espacios múltiples entre palabras.";
+        return;
+    }
+
+    const palabras = cadena.split(" ").filter(p => p.length > 0);
+    if (palabras.length < 2) {
+        document.getElementById("resultadoC").value = "Debes ingresar al menos dos palabras.";
+        return;
+    }
+
+    const resultado = palabras.reverse().join(" ");
+    document.getElementById("resultadoC").value = resultado;
+}
+
+function Borrar1(){
+    document.getElementById("resultadoC").value = "";
+    document.getElementById("p1-input").value = "";
 }
 
 
