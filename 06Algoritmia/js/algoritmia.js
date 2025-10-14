3768a3bd7fbdd5999185dade61d3d501ee7a7764
5IV8_RamirezDiazLuzKaren_PWP <lramirezd2302@alumno.ipn.mx>
2025-10-13 19:24:22 -0600

    se resolvio el problema 2 de algoritmia

--- 06Algoritmia/js/algoritmia.js
+++ 06Algoritmia/js/algoritmia.js
@@ -22,7 +22,7 @@ function problema1() {
         return;
     }
 
-    const palabras = cadena.split(" ");
+    const palabras = cadena.split(" "); //split espacio
     const resultado = palabras.reverse().join(" ");
     document.getElementById("p1-output").textContent = resultado;
 }
@@ -30,7 +30,34 @@ function problema1() {
 
 
 function problema2() {
+    var p2_x1 = document.querySelector("#p2_x1").value;
+    var p2_x2 = document.querySelector("#p2_x2").value;
+    var p2_x3 = document.querySelector("#p2_x3").value;
+    var p2_x4 = document.querySelector("#p2_x4").value;
+    var p2_x5 = document.querySelector("#p2_x5").value;
+
+    var p2_y1 = document.querySelector("#p2_y1").value;
+    var p2_y2 = document.querySelector("#p2_y2").value;
+    var p2_y3 = document.querySelector("#p2_y3").value;
+    var p2_y4 = document.querySelector("#p2_y4").value;
+    var p2_y5 = document.querySelector("#p2_y5").value;
+
+    //creamos los vectores
+    var v1 =[p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+    var v2 =[p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+
+    v1 = v1.sort(function(a, b){return a - b});
+    v2 = v2.sort(function(a, b){return a - b});
+
+    v2 = v2.reverse();
+
+    var p2_producto = 0;
+    for (var i=0; i< v1.length; i++){
+
+        p2_producto += v1[i] * v2[i];
+    }
     
+    document.querySelector("#p2_resultado").textContent = "El producto escalar  minimo es: " + p2_producto;
 
 }
 
