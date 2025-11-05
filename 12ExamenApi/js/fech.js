4d1902b4d735a795c78ea5225bda292d434a6cc1
Luz Ramirez <lramirezd2302@alumno.ipn.mx>
2025-11-05 03:55:49 -0600

    se sigue creando el js y se descargaron las fuentes, imagenes, desarrollando el css

--- 12ExamenApi/js/fech.js
+++ 12ExamenApi/js/fech.js
@@ -17,18 +17,18 @@ const libro = () =>{
     const imageTemplate = "<img class='perdisplay' src='{imgSrc}' alt='perdisplay'/>";
     //imagees para errores o loading
     const images = {
-        imgPokemonNotFound: "./img/404.png",
-        imgLoading: "./img/loading.gif",
+        imgPerNotFound: "./img/404.jpg",
+        imgLoading: "./img/loading.jpg",
     };
 
     const containers = {
         imageContainer: document.getElementById("perdisplay-container"),
-        pokemonTypesContainer: document.getElementById("perVision"),
-        pokemonNameElement: document.getElementById("perNameResult"),
-        pokemonAbilitiesElement: document.getElementById("perSkillTalents"),
-        pokemonMovesElement: document.getElementById("perPassiveTalents"),
-        pokemonMovesElement: document.getElementById("perConstellations"),
-        pokemonIdElement: document.getElementById("perId")
+        perVisionContainer: document.getElementById("perVision"),
+        perNameElement: document.getElementById("perNameResult"),
+        perSkillTalentsElement: document.getElementById("perSkillTalents"),
+        perPassiveTalentsElement: document.getElementById("perPassiveTalents"),
+        perConstellationsElement: document.getElementById("perConstellations"),
+        perIdElement: document.getElementById("perId")
     };
 
     const buttons = {
@@ -66,39 +66,123 @@ const libro = () =>{
     };
 
     const processPerData = (perDatas) => {
-        
+
         perDatas.stats?.forEach((perData) => {
             // Evalua el nombre de la estadística, y coloca su valor en su respectivo contenedor, y le aplica un
             // estilo de gradiente, para hacer más visual el efecto.
-            switch (pokemonStatData.stat.name) {
-                case "hp":
-                    pokemonStatsElements.hp.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.hp.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+            switch (perData.stat.name) {
+                case "weapon":
+                    const weaponType = perData.weapon_type.toLowerCase();
+                    const weaponImgUrl = `https://genshin.dev/weapons/${weaponType}/icon.png`;
+                    perDataElements.weapon.innerHTML = `<img src="${weaponImgUrl}" alt="${perData.weapon}" width="32" height="32"> ${perData.weapon}`;
                     break;
-                case "attack":
-                    pokemonStatsElements.attack.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.attack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+
+                case "nation":
+                    const nationKey = perData.nation.toLowerCase();
+                    const nationImgUrl = `https://genshin.dev/nations/${nationKey}/icon.png`;
+                    perDataElements.nation.innerHTML = `<img src="${nationImgUrl}" alt="${perData.nation}" width="32" height="32"> ${perData.nation}`;
                     break;
-                case "defense":
-                    pokemonStatsElements.defense.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.defense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+
+                case "affiliation":
+                    perDataElements.affiliation.innerHTML = perData.affiliation;
                     break;
-                case "special-attack":
-                    pokemonStatsElements.specialAttack.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.specialAttack.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+
+                case "rarity":
+                    const stars = "🟆".repeat(perData.rarity);
+                    perDataElements.rarity.innerHTML = `${stars} (${perData.rarity})`;
                     break;
-                case "special-defense":
-                    pokemonStatsElements.specialDefense.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.specialDefense.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+
+                case "birthday":
+                    perDataElements.birthday.innerHTML = perData.birthday !== "0000-00-00"
+                            ? perData.birthday
+                            : "Sin registro";
                     break;
-                case "speed":
-                    pokemonStatsElements.speed.innerHTML = pokemonStatData.base_stat;
-                    pokemonStatsElements.speed.style = `background: linear-gradient(0deg, rgba(0,118,255,1) ${pokemonStatData.base_stat}%, rgba(0,0,0,1) ${pokemonStatData.base_stat}%); `;
+
+                case "constellation":
+                    perDataElements.constellation.innerHTML = perData.constellation;
                     break;
             }
         });
     };
-    
 
+    const processPerSkillTalents = (perData) => {
+        let perSkillTalentsContent = "";
+        perData.skillTalents?.forEach((perSkillTalents) => {
+            perSkillTalentsContent += `<li>${perSkillTalents.skillTalents.name}</li>`;
+        });
+        containers.perSkillTalentsElement.innerHTML = perSkillTalentsContent;
+    };
+
+    const processPerPassiveTalents = (perData) => {
+        let perPassiveTalentsContent = "";
+        perData.passiveTalents?.forEach((perPassiveTalents) => {
+            perPassiveTalentsContent += `<li>${perPassiveTalents.passiveTalents.name}</li>`;
+        });
+        containers.perPassiveTalentsElement.innerHTML = perPassiveTalentsContent;
+    };
+
+    const processPerConstellations = (perData) => {
+        let perConstellationsContent = "";
+        perData.constellations?.forEach((perConstellations) => {
+            perConstellationsContent += `<li>${perConstellations.constellations.name}</li>`;
+        });
+        containers.perConstellationsElement.innerHTML = perConstellationsContent;
+    };
+//cargar las imagenes
+    const setLoading = () => {
+        containers.imageContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgLoading);
+        buttons.all.forEach(button => button.disabled = true);
+    };
+    // Vuelve a habilitar los botones
+    const setLoadingComplete = () => {
+        buttons.all.forEach(button => checkDisabled(button));
+    };
+    
+    const getPokemonData = async (pokemonName) => fetch(`${pokeApiUrl}pokemon/${pokemonName}`, {
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then((res) => res.json())
+        .catch((error) => ({requestFailed: true}));
+    
+    const checkDisabled = (button) => {
+        button.disabled = button.id === "btnDown" && +containers.perIdElement.value <= 1;
+    };
+
+    const setPersonajeData = async (perName) => {
+        if (perName) {
+            setLoading();
+
+            const perDatas = await getPersonajeData(typeof perName === typeof "" ? perName.toLowerCase() : perName);
+            if (perDatas.requestFailed) {
+                // Si no se encontró el pokemon, se pone la imagen de no encontrado
+                containers.imageContainer.innerHTML = imageTemplate.replace("{imgSrc}", images.imgPerNotFound);
+            } else {
+                // Pone las imágenes del pokemon, su nombre y el ID del pokemon
+                containers.imageContainer.innerHTML = `${imageTemplate.replace("{imgSrc}", perDatas.sprites.front_default)}
+                `;
+                containers.pokemonNameElement.innerHTML = pokemonData.name;
+                containers.pokemonIdElement.value = pokemonData.id;
+                // reparte el resto de procesamientos pertinentes a cada función
+                processPokemonTypes(pokemonData);
+                processPokemonStats(pokemonData);
+                processPokemonAbilities(pokemonData);
+                processPokemonMoves(pokemonData);
+            }
+            // vuelve a habilitar los botones.
+            setLoadingComplete();
+        } else {
+            // Esta es la forma de utilizar SweetAlert 2, por si te interesa aprender más sobre su uso puedes revisar su
+            // sitio oficial https://sweetalert2.github.io/ pero no es necesario.
+            Swal.fire({
+                title: "Error!",
+                text: "Ingresa el nombre de un pokémon primero",
+                icon: "error",
+                confirmButtonText: "Aceptar.",
+            });
+        }
+    };
 
 };
